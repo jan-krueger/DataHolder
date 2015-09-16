f854eb89761147c72d68d5618ebf64709fe1f2ba
sweetcode <jan@krueger-jan.de>
2015-09-16 17:52:10 +0200

    fixed some unit tests

--- src/test/java/DataManagerTest.java
+++ src/test/java/DataManagerTest.java
@@ -8,50 +8,31 @@ import org.junit.Assert;
 
 public class DataManagerTest extends TestCase {
 
-    private DataHolder dataHolder = new DataHolder();
-
     public DataManagerTest() {
         super("DataManagerTest");
     }
 
     public void testDefaultObject() {
 
+        DataHolder dataHolder = new DataHolder();
+
         Assert.assertEquals(
                 "Unexpected properties stored in the default DataHolder.",
                 true,
-                this.dataHolder.getProperties().isEmpty()
+                dataHolder.getProperties().isEmpty()
         );
 
     }
 
     public void testStoringObject() {
 
-        this.dataHolder.store(new ExampleProperty());
+        DataHolder dataHolder = new DataHolder();
+        dataHolder.store(new ExampleProperty());
 
         Assert.assertEquals(
                 "Couldn't store the Property.",
                 true,
-                this.dataHolder.getProperty(ExampleProperty.class).get().get().isPresent()
-        );
-
-    }
-
-    public void testIfTheExampleIsStillStored() {
-
-        Assert.assertEquals(
-                "Property isn't stored.",
-                "Example Property Value",
-                this.dataHolder.getProperty(ExampleProperty.class).get().get().get()
-        );
-
-    }
-
-    public void testStoredPropertyTypes() {
-
-        Assert.assertEquals(
-                "Couln't return all Property types.",
-                true,
-                (this.dataHolder.getPropertieTypes().size() == 1)
+                dataHolder.getProperty(ExampleProperty.class).get().get().isPresent()
         );
 
     }
