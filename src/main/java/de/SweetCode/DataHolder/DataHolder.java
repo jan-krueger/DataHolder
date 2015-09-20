34b8399ca7ec8e26147e99d7b8b1a1dbb4f28e50
sweetcode <jan@krueger-jan.de>
2015-09-20 23:30:39 +0200

    Delete Methods     + Added a lot of delete methods to the DataHolder class.     + Added unit tests for the delete methods.     + Updated some other methods to return the right data type.

--- src/main/java/de/SweetCode/DataHolder/DataHolder.java
+++ src/main/java/de/SweetCode/DataHolder/DataHolder.java
@@ -21,7 +21,7 @@ public class DataHolder {
      */
     public boolean store(Property<?, ?> property) {
 
-        if(!(this.getProperty(property.getClass(), property.getKey()) == null)) {
+        if(this.contains(property.getClass(), property.getKey())) {
             return false;
         }
 
@@ -73,15 +73,15 @@ public class DataHolder {
      * @param propertyClass The property class which implements the {@see de.SweetCode.DataHolder.Property.Property} interface.
      * @return T the result, if no property is stored for the class the function will return null.
      */
-    public <T extends Property<?, ?>> Property<?, ?> getProperty(Class<T> propertyClass, Object key) {
+    public <T extends Property<?, ?>> T getProperty(Class<T> propertyClass, Object key) {
 
-        List<Property<?, ?>> list = this.getProperties(propertyClass);
+        List<T> list = this.getProperties(propertyClass);
 
         if(list.isEmpty()) {
             return null;
         }
 
-        for(Property<?, ?> entry : this.getProperties(propertyClass)) {
+        for(T entry : this.getProperties(propertyClass)) {
 
             if(entry.getKey().equals(key)) {
                 return entry;
@@ -97,15 +97,101 @@ public class DataHolder {
      * Returns all stored Properties.
      * @param propertyClass The property class which implements the {@see de.SweetCode.DataHolder.Property.Property} interface.
      * @param <T>
-     * @return Collection<T> A collection of all stored Properties of the same type.
+     * @return Collection<T> A collection of all stored Properties of the same type if no Property is stored for the given class the function will return a empty ArrayList.
      */
-    public <T extends Property<?, ?>> List<Property<?, ?>> getProperties(Class<T> propertyClass) {
+    public <T extends Property<?, ?>> List<T> getProperties(Class<T> propertyClass) {
 
         if(!(this.datas.containsKey(propertyClass))) {
             return new ArrayList<>();
         }
 
-        return this.datas.get(propertyClass);
+        return (List<T>) this.datas.get(propertyClass);
+
+    }
+
+    /**
+     * Returns the amount of the stored Properties
+     * @return
+     */
+    public int size() {
+        return this.datas.size();
+    }
+
+    /**
+     * Checks if the DataHolder contains the given Property class.
+     * @param propertyClass The Property class
+     * @param <T>
+     * @return true if the DataHolder contains the Property class.
+     */
+    public <T extends Property<?, ?>> boolean contains(Class<T> propertyClass) {
+
+        return (!(this.datas.get(propertyClass) == null));
+
+    }
+
+    /**
+     * Checks if the DataHolder contains the given Property class and if the class is related with the given key.
+     * @param propertyClass The Property class
+     * @param <T>
+     * @return true if the DataHolder contains the Property class with the related key.
+     */
+    public <T extends Property<?, ?>> boolean contains(Class<T> propertyClass, Object key) {
+
+        return (!(this.getProperty(propertyClass, key) == null));
+
+    }
+
+    /**
+     * Delets a Property that is related with a key.
+     * @param propertyClass The Property class.
+     * @param key The related key.
+     * @param <T>
+     * @return returns the deleted Property object if the DataHolder contains the given pair of Property class and key otherwise it returns null.
+     */
+    public <T extends Property<?, ?>> T deleteProperty(Class<T> propertyClass, Object key) {
+
+        if(!(this.contains(propertyClass, key))) {
+            return null;
+        }
+
+        T property = this.getProperty(propertyClass, key);
+        this.deleteProperty(property);
+        return property;
+
+    }
+
+    /**
+     * Delets a Property.
+     * @param property The Property object.
+     * @param <T>
+     * @return returns the deleted Property object if the DataHolder contains the given pair of Property class and key otherwise it returns null.
+     */
+    public <T extends Property<?, ?>> T deleteProperty(T property) {
+
+        if(!(this.contains(property.getClass(), property.getKey()))) {
+            return null;
+        }
+
+        this.datas.remove(property.getClass(), property);
+        return property;
+
+    }
+
+    /**
+     * Delets all Properties related to this Property class.
+     * @param <T>
+     * @param propertyClass The Property class.
+     * @return returns a list of all deleted Properties.
+     */
+    public <T extends Property<?, ?>> List<T> deleteProperties(Class<T> propertyClass) {
+
+        if(!(this.contains(propertyClass))) {
+            return null;
+        }
+
+        List<T> properties = this.getProperties(propertyClass);
+        this.datas.removeAll(propertyClass);
+        return properties;
 
     }
 
---
+++ src/test/java/DataManagerDeleteTest.java
@@ -0,0 +1,102 @@
+import de.SweetCode.DataHolder.DataHolder;
+import de.SweetCode.DataHolder.Property.properties.IntegerProperty;
+import de.SweetCode.DataHolder.Property.properties.StringProperty;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by Yonas on 20.09.2015.
+ */
+public class DataManagerDeleteTest extends TestCase {
+
+    @Test
+    public void testSingleObjectPropertyDelete() {
+
+        DataHolder dataHolder = new DataHolder();
+
+        StringProperty<String> objectA = new StringProperty<String>("A", "Yonas");
+        dataHolder.store(objectA);
+
+        StringProperty<String> objectB = new StringProperty<String>("B", "Felix");
+        dataHolder.store(objectB);
+
+
+        StringProperty<String> deletedProperty = dataHolder.deleteProperty(objectA);
+
+        Assert.assertEquals(
+                "Deleted Property is null, should be a StringProperty.",
+                true,
+                (!(deletedProperty == null))
+        );
+
+        Assert.assertEquals(
+                "Property hasn't been removed.",
+                true,
+                (dataHolder.size() == 1)
+        );
+
+    }
+
+    @Test
+    public void testSingleClassAndKeyPropertyDelete() {
+
+        DataHolder dataHolder = new DataHolder();
+
+        StringProperty<String> objectA = new StringProperty<String>("A", "Yonas");
+        dataHolder.store(objectA);
+
+        StringProperty<String> objectB = new StringProperty<String>("B", "Felix");
+        dataHolder.store(objectB);
+
+
+        StringProperty<String> deletedProperty = dataHolder.deleteProperty(StringProperty.class, "A");
+
+        Assert.assertEquals(
+                "Deleted Property is null, should be a StringProperty.",
+                true,
+                (!(deletedProperty == null))
+        );
+
+        Assert.assertEquals(
+                "Property hasn't been removed.",
+                true,
+                (dataHolder.size() == 1)
+        );
+
+    }
+
+    @Test
+    public void testMultiObjectPropertyDelete() {
+
+        DataHolder dataHolder = new DataHolder();
+
+        StringProperty<String> objectA = new StringProperty<String>("A", "Yonas");
+        dataHolder.store(objectA);
+
+        StringProperty<String> objectB = new StringProperty<String>("B", "Felix");
+        dataHolder.store(objectB);
+
+        IntegerProperty<String> objectC = new IntegerProperty<String>("Money", 14);
+        dataHolder.store(objectC);
+
+
+        List<StringProperty> deletedList =  dataHolder.deleteProperties(StringProperty.class);
+
+        Assert.assertEquals(
+                String.format("Amount of deleted Properties is wrong. %d instead of 2.", deletedList.size()),
+                true,
+                (!(deletedList.size() == 2))
+        );
+
+        Assert.assertEquals(
+                "Removed not only the expected Properties.",
+                true,
+                (dataHolder.size() == 1)
+        );
+
+    }
+
+}
